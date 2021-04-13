4005fcb98db2e00f6adaded5d54058c5f2af8381
James Briggs <jfbriggs106@gmail.com>
2021-04-13 15:02:21 -0700

    Add code to frontend main

--- frontend/main.py
+++ frontend/main.py
@@ -1,2 +1,25 @@
-import streamlit
+import streamlit as st
+import requests
 
+
+def get_info():
+    data = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
+    return data
+
+
+def main():
+    st.title("Hello, just testing.")
+
+    st.subheader("Also a test.")
+
+    data = get_info()
+
+    st.write(data)
+
+    st.balloons()
+
+    st.write("Goodbye!")
+
+
+if __name__ == "__main__":
+    main()
